9eea8eafd009e662163e70627a63af7f59ea562f
vanessakist <vanesasilva_sc@hotmail.com>
2023-04-27 01:32:32 +0200

    norme

--- philo/sources/philo.c
+++ philo/sources/philo.c
@@ -3,40 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkist-si <vkist-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 20:12:09 by vkist-si          #+#    #+#             */
-/*   Updated: 2023/04/26 18:46:58 by vkist-si         ###   ########.fr       */
+/*   Updated: 2023/04/27 00:17:51 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static void clean(t_data *data)
+static void	clean(t_data *data)
 {
 	pthread_mutex_destroy(&data->mutex_last_meal);
-  pthread_mutex_destroy(&data->mutex_stop);
-  pthread_mutex_destroy(&data->mutex_meals);
-  pthread_mutex_destroy(&data->mutex_monitor);
-  pthread_mutex_destroy(&data->mutex_print);
-  pthread_mutex_destroy(&data->mutex_flag);
+	pthread_mutex_destroy(&data->mutex_stop);
+	pthread_mutex_destroy(&data->mutex_meals);
+	pthread_mutex_destroy(&data->mutex_monitor);
+	pthread_mutex_destroy(&data->mutex_print);
+	pthread_mutex_destroy(&data->mutex_flag);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-  t_data  *data;
-  t_philo *philo;
+	t_data	*data;
+	t_philo	*philo;
 
-  if (argc < 5 || argc > 6)
-    return (0);
-  if (check_input(argc, argv))
-    return (1);
-  data = new_data(argv);
-  philo = new_philo(data, argv, argc, 0);
-  create_thread(data, argv, philo);
-  free_forks(data);
-  clean(data);
-  free(philo);
-  free(data);
-  return (0);
+	if (argc < 5 || argc > 6)
+		return (0);
+	if (check_input(argc, argv))
+		return (1);
+	data = new_data(argv);
+	philo = new_philo(data, argv, argc, 0);
+	create_thread(data, argv, philo);
+	free_forks(data);
+	clean(data);
+	free(philo);
+	free(data);
+	return (0);
 }
--- philo/sources/threads.c
+++ philo/sources/threads.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   threads.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vkist-si <vkist-si@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 00:18:50 by vkist-si          #+#    #+#             */
-/*   Updated: 2023/04/26 19:00:21 by vkist-si         ###   ########.fr       */
+/*   Updated: 2023/04/27 00:13:53 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void    create_thread(char **argv, t_philo *philo)
+void	create_thread(char **argv, t_philo *philo)
 {
-    int             i;
-    pthread_t       *thread;
-	pthread_t       monitoring_thread;
+	pthread_t	*thread;
+	pthread_t	monitoring_thread;
+	int			i;
 
-    thread = ft_calloc(sizeof(pthread_t), ft_atoi(argv[1]));
-    i = 0;
-    while (i < ft_atoi(argv[1]))
-    {
-        pthread_create(&thread[i], NULL, routine, &philo[i]);
-        i++;
-    }
-    i = 0;
-    pthread_create(&monitoring_thread, NULL, check_death, philo);
-    while (i < ft_atoi(argv[1]))
-    {
-        pthread_join(thread[i], NULL);
-        i++;
-    }
-    pthread_join(monitoring_thread, NULL);
-    free(thread);
+	thread = ft_calloc(sizeof(pthread_t), ft_atoi(argv[1]));
+	i = 0;
+	while (i < ft_atoi(argv[1]))
+	{
+		pthread_create(&thread[i], NULL, routine, &philo[i]);
+		i++;
+	}
+	i = 0;
+	pthread_create(&monitoring_thread, NULL, check_death, philo);
+	while (i < ft_atoi(argv[1]))
+	{
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	pthread_join(monitoring_thread, NULL);
+	free(thread);
 }
